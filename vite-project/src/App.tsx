d73b9b7a303834458d3353e79b43ee454b507480
GABRIEL MENDES FONSECA <124696627+Gmendesfonseca@users.noreply.github.com>
2024-04-12 11:23:20 -0300

    Update

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,36 +1,94 @@
-import { Sidebar } from './components/Sidebar/Sidebar'
-import { Feed } from './components/Feed/Feed'
-import { Rightbar } from './components/Rightbar/Rightbar'
-import { Box, createTheme, PaletteMode, Stack, ThemeProvider, useMediaQuery } from '@mui/material'
-import { Navbar } from './components/NavBar/Navbar'
-import { Add } from './components/Add/Add'
-import React, { useState } from 'react'
+import { Sidebar } from './components/MainPage/Sidebar/Sidebar';
+import { Feed } from './components/MainPage/Feed/Feed';
+import { Rightbar } from './components/MainPage/Rightbar/Rightbar';
+import { Navbar } from './components/MainPage/NavBar/Navbar';
+import { Add } from './components/MainPage/Add/Add';
+import {
+  Box,
+  createTheme,
+  PaletteMode,
+  Stack,
+  ThemeProvider,
+  useMediaQuery,
+} from '@mui/material';
+import React, { useState } from 'react';
+import SignInSide from './components/loginPage/SignInSide';
 
-type View = 'login' | 'home' | 'profile' | 'register' | 'settings' | 'chat' | 'bookmarks';
+type View = 'login' | 'home' | 'profile' | 'register' | 'settings' | 'chat';
 
 function App() {
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: light)')
-  const [mode, setMode] = useState<PaletteMode>(prefersDarkMode ? 'dark' : 'light')
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: light)');
+  const [mode, setMode] = useState<PaletteMode>(
+    prefersDarkMode ? 'light' : 'dark',
+  );
+
+  const [view, setView] = useState<View>('login');
+
+  const renderView = () => {
+    switch (view) {
+      case 'home':
+        return <HomeComponent />;
+      case 'profile':
+        return <ProfileComponent />;
+      case 'register':
+        return <RegisterComponent />;
+      case 'settings':
+        return <SettingsComponent />;
+      case 'chat':
+        return <ChatComponent />;
+      default:
+        return <HomeComponent />;
+    }
+  };
 
   const darkTheme = createTheme({
     palette: {
-      mode: mode
-    }
-  })
+      mode: mode,
+    },
+  });
+
+  function HomeComponent() {
+    return (
+      <ThemeProvider theme={darkTheme}>
+        <Box bgcolor={'background.default'} color={'text.primary'}>
+          <Navbar />
+          <Stack direction="row" spacing={2} justifyContent="space-between">
+            <Sidebar mode={mode} setMode={setMode} />
+            <Feed />
+            <Rightbar />
+          </Stack>
+          <Add />
+        </Box>
+      </ThemeProvider>
+    );
+  }
+
+  function ProfileComponent() {
+    return <Box>About Page</Box>;
+  }
+
+  function RegisterComponent() {
+    return <Box>Contact Page</Box>;
+  }
+
+  function SettingsComponent() {
+    return <Box>About Page</Box>;
+  }
+
+  function ChatComponent() {
+    return <Box>Contact Page</Box>;
+  }
 
   return (
-    <ThemeProvider theme={darkTheme}>
-      <Box bgcolor={'background.default'} color={'text.primary'}>
-        <Navbar />
-        <Stack direction="row" spacing={2} justifyContent="space-between">
-          <Sidebar mode={mode} setMode={setMode} />
-          <Feed />
-          <Rightbar />
-        </Stack>
-        <Add />
-      </Box>
-    </ThemeProvider>
-  )
+    <div>
+      <SignInSide />
+      <button onClick={() => setView('home')}>Home</button>
+      <button onClick={() => setView('profile')}>Profile</button>
+      <button>Register</button>
+      <button onClick={() => setView('settings')}>Setings</button>
+      <button onClick={() => setView('chat')}>Chat</button>
+      {renderView()}
+    </div>
+  );
 }
-
-export default App
+export default App;
