c22d18fb29e49ab7d533bf404a32653985157341
GABRIEL MENDES FONSECA <124696627+Gmendesfonseca@users.noreply.github.com>
2024-05-08 17:59:33 -0300

    Post requests

---
+++ vite-project/src/services/posts/request.ts
@@ -0,0 +1,41 @@
+import { PostType } from './types';
+import { api } from '../api';
+
+// GET
+export function getPost(id: number): Promise<PostType> {
+  return api.get<PostType>(`/post/${id}`).then((response) => {
+    if (response.status !== 200) {
+      throw new Error('Post not found');
+    }
+    return response.data;
+  });
+}
+
+// POST
+export function createPost(postData: PostType): Promise<PostType> {
+  return api.post<PostType>('/post', postData).then((response) => {
+    if (response.status !== 201) {
+      throw new Error('Error creating post');
+    }
+    return response.data;
+  });
+}
+
+// PUT
+export function updatePost(id: number, postData: PostType): Promise<PostType> {
+  return api.put<PostType>(`/post/${id}`, postData).then((response) => {
+    if (response.status !== 200) {
+      throw new Error('Error updating post');
+    }
+    return response.data;
+  });
+}
+
+// DELETE
+export function deletePost(id: number): Promise<void> {
+  return api.delete(`/post/${id}`).then((response) => {
+    if (response.status !== 204) {
+      throw new Error('Error deleting post');
+    }
+  });
+}
---
+++ vite-project/src/services/posts/types.ts
@@ -0,0 +1,10 @@
+export type PostType = {
+  id: number;
+  title: string;
+  likes: number;
+  comments: string;
+  content: string;
+  dataPostagem: Date;
+  creatorId: number;
+  institutionId: number;
+};
