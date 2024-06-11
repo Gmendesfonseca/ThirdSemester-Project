5a8168a9d1bae41f919477b71777d52f44f86675
Jonas Moura <Jonas_jm@live.com>
2024-06-10 21:32:12 -0300

    ,

--- vite-project/src/components/HomeComponents/NavBar/Navbar.tsx
+++ vite-project/src/components/HomeComponents/NavBar/Navbar.tsx
@@ -13,48 +13,48 @@ import {
   Menu,
   IconButton,
   Tooltip,
-} from '@mui/material';
-import MailIcon from '@mui/icons-material/Mail';
-import React from 'react';
+} from "@mui/material";
+import MailIcon from "@mui/icons-material/Mail";
+import React from "react";
 import {
   Logout,
   Notifications,
   // PersonAdd,
   Settings,
-} from '@mui/icons-material';
-import { useNavigate } from 'react-router-dom';
-import { useSession } from '../../../context/SessionContext';
-import { useChat } from '../../../context/ChatsContext';
+} from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+import { useSession } from "../../../context/SessionContext";
+import { useChat } from "../../../context/ChatsContext";
 
 const StyledToolbar = styled(Toolbar)({
-  display: 'flex',
-  justifyContent: 'space-between',
+  display: "flex",
+  justifyContent: "space-between",
 });
 
-const Search = styled('div')(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  backgroundColor: 'white',
+const Search = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  backgroundColor: "white",
   borderRadius: theme.shape.borderRadius,
-  width: '40%',
-  padding: '0 10px',
+  width: "40%",
+  padding: "0 10px",
 }));
 
 const Icons = styled(Box)(({ theme }) => ({
-  display: 'flex',
-  alignItems: 'center',
-  gap: '20px',
-  [theme.breakpoints.down('sm')]: {
-    display: 'none',
+  display: "flex",
+  alignItems: "center",
+  gap: "20px",
+  [theme.breakpoints.down("sm")]: {
+    display: "none",
   },
 }));
 
 const UserBox = styled(Box)(({ theme }) => ({
-  display: 'none',
-  alignItems: 'center',
-  gap: '10px',
-  [theme.breakpoints.down('sm')]: {
-    display: 'flex',
+  display: "none",
+  alignItems: "center",
+  gap: "10px",
+  [theme.breakpoints.down("sm")]: {
+    display: "flex",
   },
 }));
 
@@ -77,21 +77,21 @@ export const Navbar = () => {
     setInstitution(null);
     setUser(null);
     setChatList([]);
-    navigate('/login');
+    navigate("/login");
   };
 
   const handleProfile = () => {
-    navigate('/profile');
+    navigate("/profile");
   };
 
   const handleSettings = () => {
-    navigate('/settings');
+    navigate("/settings");
   };
 
   return (
-    <AppBar position="sticky" sx={{ width: '100%' }}>
+    <AppBar position="sticky" sx={{ width: "100%", bgcolor: "#011824" }}>
       <StyledToolbar>
-        <Typography variant="h6" sx={{ xs: 'none', sm: 'block' }}>
+        <Typography variant="h6" sx={{ xs: "none", sm: "block" }}>
           InnerLink
         </Typography>
         <Search>
@@ -109,9 +109,9 @@ export const Navbar = () => {
               onClick={handleClick}
               size="small"
               sx={{ ml: 2 }}
-              aria-controls={open ? 'account-menu' : undefined}
+              aria-controls={open ? "account-menu" : undefined}
               aria-haspopup="true"
-              aria-expanded={open ? 'true' : undefined}
+              aria-expanded={open ? "true" : undefined}
             >
               <Avatar
                 sx={{ width: 32, height: 32 }}
@@ -126,9 +126,9 @@ export const Navbar = () => {
               onClick={handleClick}
               size="small"
               sx={{ ml: 2, gap: 1 }}
-              aria-controls={open ? 'account-menu' : undefined}
+              aria-controls={open ? "account-menu" : undefined}
               aria-haspopup="true"
-              aria-expanded={open ? 'true' : undefined}
+              aria-expanded={open ? "true" : undefined}
             >
               <Avatar
                 sx={{ width: 32, height: 32 }}
@@ -150,31 +150,31 @@ export const Navbar = () => {
         PaperProps={{
           elevation: 0,
           sx: {
-            overflow: 'visible',
-            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            overflow: "visible",
+            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
             mt: 1.5,
-            '& .MuiAvatar-root': {
+            "& .MuiAvatar-root": {
               width: 32,
               height: 32,
               ml: -0.5,
               mr: 1,
             },
-            '&::before': {
+            "&::before": {
               content: '""',
-              display: 'block',
-              position: 'absolute',
+              display: "block",
+              position: "absolute",
               top: 0,
               right: 14,
               width: 10,
               height: 10,
-              bgcolor: 'background.paper',
-              transform: 'translateY(-50%) rotate(45deg)',
+              bgcolor: "background.paper",
+              transform: "translateY(-50%) rotate(45deg)",
               zIndex: 0,
             },
           },
         }}
-        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
       >
         {/* <MenuItem onClick={handleClose}>
           <Avatar /> Profile
