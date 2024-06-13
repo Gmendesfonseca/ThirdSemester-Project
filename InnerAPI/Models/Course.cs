f618f58ca5233d05d9e86427fe0c81dce5f1cb1f
NicollyNOA <152316760+NicollyNOA@users.noreply.github.com>
2024-06-13 01:26:00 -0300

    commit

---
+++ InnerAPI/Models/Course.cs
@@ -0,0 +1,18 @@
+﻿namespace InnerAPI.Models
+{
+    public class Course
+    {
+        public uint Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Credits { get; set; }
+
+        public Course(uint id, string name, string description, int credits)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            Credits = credits;
+        }
+    }
+}
