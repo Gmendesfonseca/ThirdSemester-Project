b2da251cecb9816f12c9436e18c63b427d78b220
GABRIEL MENDES FONSECA <124696627+Gmendesfonseca@users.noreply.github.com>
2024-06-14 07:29:15 -0300

    Update ListCourses.tsx

--- vite-project/src/pages/Lists/ListCourses.tsx
+++ vite-project/src/pages/Lists/ListCourses.tsx
@@ -55,7 +55,7 @@ export default function ListCourse() {
   return (
     <ThemeProvider theme={darkTheme}>
       <Box bgcolor={'background.default'} color={'text.primary'}>
-        <NavBar />
+        <NavBar navAct="InnerLink" />
         <Stack direction="row" spacing={2} justifyContent="space-between">
           <SidebarMenu />
           <InTable<CourseListType>
